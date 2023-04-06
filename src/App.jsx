25eaa2fdf69f254bad86cebae2f2d9035c9b700b
Aman Kumar Singh <amankumar7355197337@gmail.com>
2023-04-06 19:20:15 +0530

    adding loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
+import Cat from './Cat'
 import Footer from './Footer'
-const url = 'https://api.thecatapi.com/v1/images/search?limit=10'
 
 const cats = () => {
-  const [cats, setCats] = useState([])
 
-  useEffect(() => {
-    // console.log('useEffect Set Up')
-    const getData = async (url) => {
-      const resp = await fetch(url)
-      const data = await resp.json()
-      setCats(data)
-      // console.log(data)
-    }
-    getData(url)
-  }, [])
 
   const handleRefresh = () => {
     window.location.reload()
@@ -30,16 +18,7 @@ const cats = () => {
             <span className="material-symbols-outlined" title='refresh'>refresh</span>
           </button>
         </div>
-        <ul className="cats">
-          {cats.map((cat) => {
-            const { id, url } = cat
-            return (
-              <li key={id} className="cat">
-                <img src={url} alt="cat" className="catImg" />
-              </li>
-            )
-          })}
-        </ul>
+        <Cat />
       </section>
       <Footer />
     </>
---
+++ src/Cat.jsx
@@ -0,0 +1,54 @@
+import React, { useState, useEffect } from 'react'
+const url = 'https://api.thecatapi.com/v1/images/search?limit=10'
+
+const Cat = () => {
+  const [cats, setCats] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    // console.log('useEffect Set Up')
+    setTimeout(() => {
+      const getData = async (url) => {
+        const resp = await fetch(url)
+        const data = await resp.json()
+        setCats(data)
+        // console.log(data)
+      }
+      getData(url)
+      setIsLoading(false)
+    }, 3000)
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className="lds-default">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    )
+  }
+  return (
+    <ul className="cats">
+      {cats.map((cat) => {
+        const { id, url } = cat
+        return (
+          <li key={id} className="cat">
+            <img src={url} alt="cat" className="catImg" />
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+export default Cat
